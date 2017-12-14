1b1697c8016d4c1fc0ba92b63dbed74691cd9657
Joey Manso <jmanso522@gmail.com>
2017-12-13 22:27:37 -0600

    Completion of Day 1

--- Source/Day01.cpp
+++ Source/Day01.cpp
@@ -1,14 +1,56 @@
 #include "Days.h"
+#include "AdventMath.h"
 
 template <>
 void Run<Day01>(Part part, istream& is, ostream& os)
 {
     if(part == Part01)
     {
+		int sum = 0;
 
+		string arg;
+		while(getline(is, arg, ','))
+		{
+			if(arg.length() > 1)
+			{
+				for(int i = 0; i < arg.length(); ++i)
+				{
+					// First index should compare to last, all others should compare to previous
+					size_t j = (i == 0) ? arg.length() - 1 : i - 1;
+					if(arg[i] == arg[j])
+					{
+						sum += ToInt(arg[i]);
+					}
+				}
+			}
+		}
+		os << sum;
     }
     else if(part == Part02)
     {
+		int sum = 0;
 
+		string arg;
+		while(getline(is, arg, ','))
+		{
+			if(arg.length() > 1 && arg.length() % 2 == 0)
+			{
+				size_t step = (arg.length() / 2);
+				for(int i = 0; i < arg.length(); ++i)
+				{
+					// Adjust j to wrap around
+					size_t j = i + step;
+					if(j >= arg.length())
+					{
+						j -= arg.length();
+					}
+					if(arg[i] == arg[j])
+					{
+						sum += ToInt(arg[i]);
+					}
+				}
+			}
+		}
+		os << sum;
     }
 }
