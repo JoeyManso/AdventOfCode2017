90cb381af978728bf932d2b122c22bc3d6a0707a
Joey Manso <jmanso522@gmail.com>
2018-05-15 17:27:58 -0500

    Completion of Day 5

--- Source/Day05.cpp
+++ Source/Day05.cpp
@@ -1,14 +1,39 @@
 #include "Days.h"
 
+#include <vector>
+
 template <>
 void Run<Day05>(Part part, istream& is, ostream& os)
 {
-	if(part == Part01)
-	{
+	vector<int> instructions;
 
-	}
-	else if(part == Part02)
+	string arg;
+	while(getline(is, arg))
 	{
-
+		// Parse each instruction into the vector
+		int i = atoi(arg.c_str());
+		instructions.push_back(i);
 	}
+
+	// Loop through instructions using rules of each part
+	int numSteps = -0;
+	int idx = 0;
+	int offset = 0;
+	int jumpDelta = 0;
+	while(idx >= 0 && idx < instructions.size())
+	{
+		offset = instructions[idx];
+		if(part == Part01 || offset < 3)
+		{
+			jumpDelta = 1;
+		}
+		else
+		{
+			jumpDelta = -1;
+		}
+		instructions[idx] += jumpDelta;
+		idx += offset;
+		++numSteps;
+	}
+	cout << "numSteps: " << numSteps;
 }
